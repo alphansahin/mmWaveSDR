e3ed9948bcd2ec7803e52cf0b4b8bfd6bbae8058
Alphan Sahin <45111014+alphansahin@users.noreply.github.com>
2023-01-06 11:08:26 -0500

    Delete designIPs directory

--- designIPs/fcn_rrc.m
+++
@@ -1,6 +0,0 @@
-function h = fcn_rrc(beta,t)
-h = (sin((1-beta)*pi*t)+4*beta*t.*cos((1+beta)*pi*t))./ (pi*t.*(1-(4*beta*t).^2));
-h(t==0) = 1 - beta*(1 - (4/pi));
-h(t == 1/4/beta | t == -1/4/beta) = beta/sqrt(2)*((1+2/pi) * sin(pi/(4*beta)) + (1-2/pi) * cos(pi/(4*beta)));
-h = h(:);
-end
--- designIPs/fcn_singleCarrier.m
+++
@@ -1,9 +0,0 @@
-function [IQdata, h] =  fcn_singleCarrier(rho, symbols, Nover)
-    n = [-15:1/Nover:15];
-    h = fcn_rrc(rho,n);
-    %h = sinc(n);
-    h = h/norm(h);
-    h = h(:);
-    symbolsUp = upsample(symbols(:),Nover);
-    IQdata = conv(h,symbolsUp);
-end
