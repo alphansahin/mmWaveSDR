20bc2568f501d87ef7e70b16fe8df7a8f2c317ce
apondey <125098203+apondey@users.noreply.github.com>
2023-05-05 11:27:23 -0400

    Add files via upload

---
+++ design_pynq_28GHz/Test_python_24GHz/mainDefaults.py
@@ -0,0 +1,7 @@
+parametersDefault = {'Nstart': 170, \
+                     'fs': 1536e6, \
+                     #'fc': 65.34e9
+                     'fc': 24.00e9
+                     }
+
+parametersPPDU = {'lowPAPRWaveformPowerBoostdB': 1} 
